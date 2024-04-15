c609da27144e2ef318fedde199fdc4bbda59c546
chrialge <chrialge@gmail.com>
2024-04-15 15:34:41 +0200

    add style and data of the arry on html

--- index.php
+++ index.php
@@ -42,25 +42,55 @@ $hotels = [
 ];
 
 
-var_dump($hotels)
-
 ?>
 
 
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel</title>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
 <body>
 
+    <main id="site_main" style="height: 100vh;" class="d-flex align-items-center justify-content-center bg-success">
+        <div class="container bg-secondary rounded-5 p-4">
+            <h1 class="text-center mb-4">Hotels</h1>
+            <div class="row gy-4">
+
+                <?php foreach ($hotels as $key => $hotel) : ?>
+                    <div class="col-4">
+                        <div class="card" style="width:18rem;">
+                            <img src="./hotel-img.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $hotel['name'] ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted ">Vote: <?= $hotel['vote'] ?></h6>
+                                <p class="card-text">Distance to center: <?= $hotel['distance_to_center'] ?></p>
+                                <p class="card-text">Description: <?= $hotel['description'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+
+                <?php endforeach; ?>
+            </div>
 
 
+
+
+        </div>
+    </main>
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
